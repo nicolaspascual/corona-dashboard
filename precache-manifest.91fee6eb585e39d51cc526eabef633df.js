1d74b4c2e032d5fcb6250e961bcda1c184407332
nicolaspascual <nicolaspascual@users.noreply.github.com>
2020-04-14 19:37:47 +0000

    deploy: 2cfa9c477e31584eca518e1cd793445974838464

---
+++ precache-manifest.91fee6eb585e39d51cc526eabef633df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5276a050b29e31dc6597e7d05c71a67",
+    "url": "/corona-dashboard/index.html"
+  },
+  {
+    "revision": "38ec054a488fe228b76c",
+    "url": "/corona-dashboard/static/css/2.4cac4a78.chunk.css"
+  },
+  {
+    "revision": "694ba03b93713bfa4455",
+    "url": "/corona-dashboard/static/css/main.2bb02481.chunk.css"
+  },
+  {
+    "revision": "38ec054a488fe228b76c",
+    "url": "/corona-dashboard/static/js/2.af0074dc.chunk.js"
+  },
+  {
+    "revision": "f2a020ebcdd97c593a441d685b589e20",
+    "url": "/corona-dashboard/static/js/2.af0074dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "694ba03b93713bfa4455",
+    "url": "/corona-dashboard/static/js/main.f7aec440.chunk.js"
+  },
+  {
+    "revision": "9851edb9428d6ad1e851",
+    "url": "/corona-dashboard/static/js/runtime-main.dbcfaf19.js"
+  }
+]);
